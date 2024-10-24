bb21182ed72ad8bbb25dc0cda2a2eae9848f24f5
viona0209 <septianaelisa2828@gmail.com>
2024-10-24 15:50:17 +0700

    api auth, product, postman

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Category;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'viona elisa',
+            'email' => 'putri@fic17.com',
+            'password' => Hash::make('12345678')
         ]);
+
+        //category factory 2
+        Category::factory(50)->create();
+
+        //product factory 100
+        Product::factory(100)->create();
     }
 }
